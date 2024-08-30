4a2c0544cd1ceacf3885c36acf707bf3d25b9aae
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-30 22:57:15 +0800

    feat: adding type to findAll method.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Tasks } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Tasks[] = [];
 
-  findAll() {
+  findAll(): Tasks[] {
     return this.tasks;
   }
 }
