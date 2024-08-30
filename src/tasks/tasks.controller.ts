f23c3892ad5d3d67aa143c4fd3a13d96aa237cd4
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-30 23:05:19 +0800

    feat: add create function to TaskController

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Tasks } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +9,13 @@ export class TasksController {
   @Get()
   findAll(): Tasks[] {
     return this.tasksService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Tasks {
+    return this.tasksService.create(title, description);
   }
 }
