9f2e59a76d318a775f433a6b3b5ae903f21e98ca
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-31 23:10:54 +0800

    feat: wired up the TaskRepository in TaskModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
+import { Task } from './tasks.entity';
 import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksRepository],
 })
 export class TasksModule {}
