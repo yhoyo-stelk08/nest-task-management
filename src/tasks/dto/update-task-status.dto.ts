8db48980744feecab0d63f46e1311a8f9d79d19e
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-31 20:32:26 +0800

    feat: add validation in UpdateTaskStatusDto

--- src/tasks/dto/update-task-status.dto.ts
+++ src/tasks/dto/update-task-status.dto.ts
@@ -1,5 +1,8 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class UpdateTaskStatusDto {
+  @IsString()
+  @IsNotEmpty()
   status: TaskStatus;
 }
