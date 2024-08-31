69089092628bd4ea13d2b5f4aa993e0f331118cb
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-31 20:29:24 +0800

    feat: modify findOne to throw NotFoundException if no task found.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -13,8 +13,13 @@ export class TasksService {
   }
 
   findOne(id: string): Tasks {
-    console.log(id);
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return task;
   }
 
   create(createTaskDto: CreateTaskDto): Tasks {
