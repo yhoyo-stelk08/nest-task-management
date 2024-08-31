838b8a0d26d21fc3b44e9ad7605e590beceab8b8
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-31 22:37:32 +0800

    feat: create new entity class called Task

---
+++ src/tasks/tasks.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './tasks.model';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: TaskStatus,
+    default: TaskStatus.OPEN,
+  })
+  status: TaskStatus;
+}
