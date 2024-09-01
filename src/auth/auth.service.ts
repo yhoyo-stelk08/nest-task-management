c9b2603e52216d565a1291c3260248c25328abd1
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-09-01 15:00:43 +0800

    feat: add signUp method to AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UsersRepository } from './users.repository';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(UsersRepository) private userRepository: UsersRepository,
-  ) {}
+  constructor(private usersService: UsersService) {}
+
+  async signUp(createUserDto: CreateUserDto) {
+    // check if the user exist
+    try {
+      const foundUser = await this.usersService.findOneByUsername(
+        createUserDto.username,
+      );
+
+      if (foundUser) {
+        // if user exist, throw an exception
+        throw new BadRequestException('Username already exist');
+      }
+
+      // salting and hashing password process will be implemented here
+
+      // create a new user
+      const newUser = await this.usersService.createUser(createUserDto);
+      return newUser;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
 }
