b7c0d76a51dc3d9dcb2f005ce36bb094ac6bf0c2
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-09-01 14:24:16 +0800

    feat: implementing CRUD method in UsersService

--- src/auth/users.service.ts
+++ src/auth/users.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -8,4 +10,37 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(newUser);
+  }
+
+  async find(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: string): Promise<User> {
+    return await this.userRepository.findOneOrFail({ where: { id } });
+  }
+
+  async remove(id: string): Promise<User> {
+    try {
+      const user = await this.findOne(id);
+      return await this.userRepository.remove(user);
+    } catch (error) {
+      throw new NotFoundException(`User with ID "${id}" not found`);
+    }
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    try {
+      const user = await this.findOne(id);
+
+      Object.assign(user, updateUserDto);
+      return await this.userRepository.save(user);
+    } catch (error) {
+      throw new NotFoundException(`User with ID "${id}" not found`);
+    }
+  }
 }
