feb3a46a48054e7849c35ba01d11d5fbcbcc1901
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-09-01 21:53:10 +0800

    feat: use Guard to protect routes in controller. - import AuthModule in TasksModule - add AuthGuard in TasksController
     Changes to be committed:
    	modified:   src/tasks/tasks.controller.ts
    	modified:   src/tasks/tasks.module.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,13 +7,16 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
