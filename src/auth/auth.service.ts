8d59ac2774cde3b1940077fc98114574b7755b1b
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-09-01 18:52:48 +0800

    feat: implementing salting and hashing password in signUp method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -7,21 +8,25 @@ export class AuthService {
   constructor(private usersService: UsersService) {}
 
   async signUp(createUserDto: CreateUserDto) {
+    const { username, password } = createUserDto;
     // check if the user exist
     try {
-      const foundUser = await this.usersService.findOneByUsername(
-        createUserDto.username,
-      );
+      const foundUser = await this.usersService.findOneByUsername(username);
 
       if (foundUser) {
         // if user exist, throw an exception
         throw new BadRequestException('Username already exist');
       }
 
-      // salting and hashing password process will be implemented here
+      // salting and hashing password
+      const salt = await bcrypt.genSalt(); // generate a salt
+      const hashedPassword = await bcrypt.hash(password, salt); // hash the password
 
       // create a new user
-      const newUser = await this.usersService.createUser(createUserDto);
+      const newUser = await this.usersService.createUser({
+        username,
+        password: hashedPassword,
+      });
       return newUser;
     } catch (error) {
       throw new BadRequestException(error.message);
