ae3fb8888aa0e16a402934ce2ec40fd71b1764d3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-09-01 14:59:27 +0800

    feat: add validation rules to CreateUserDto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,11 +1,17 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @MinLength(4)
   username: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password need at least one uppercase letter, one lowercase letter and one number or special characters',
+  })
   password: string;
 }
